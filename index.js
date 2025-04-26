95cd904d0f1ccedde564c46a37309b75840de139
zannatin-tazri <zannatintazrizan@gmail.com>
2025-04-26 16:48:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port=process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3moahdm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    //job related apis
+    const jobsCollection=client.db('jobPortal').collection('jobs');
+
+    app.get('/jobs', async(req,res)=>{
+        const cursor=jobsCollection.find();
+        const result=await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/jobs/:id', async(req, res)=>{
+        const id=req.params.id;
+        const query={ _id: new ObjectId(id) }
+        const result= await jobsCollection.findOne(query);
+        res.send(result);
+        })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req, res)=>{
+    res.send("job is falling from the sky")
+})
+
+app.listen(port,()=>{
+    console.log(`job is waiting at port : ${port} `)
+})
