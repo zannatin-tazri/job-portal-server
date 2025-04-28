7942bffb8205c68683a84ba8b31a70462b16f9ba
zannatin-tazri <zannatintazrizan@gmail.com>
2025-04-28 18:36:21 +0600

    applicants data showed

--- index.js
+++ index.js
@@ -57,11 +57,24 @@ async function run() {
           const email=req.query.email;
           const query={applicant_email:email}
           const result = await jobApplicationCollection.find(query).toArray();
+
+          //fokira way
+          for(const application of result){
+            console.log(application.job_id);
+            const query1={_id: new ObjectId(application.job_id)}
+            const job=await jobsCollection.findOne(query1);
+            if(job){
+              application.title=job.title;
+              application.company=job.company;
+              application.company_logo=job.company_logo;
+              application.location=job.location;
+            }
+          }
           res.send(result);
         })
         app.post('/job-applications', async(req,res)=>{
           const application = req.body;
-          const result= jobApplicationCollection.insertOne(application);
+          const result=await jobApplicationCollection.insertOne(application);
           res.send(result);
         })
 
